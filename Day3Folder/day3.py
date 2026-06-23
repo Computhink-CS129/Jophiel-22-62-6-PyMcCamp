b95d8f6c40c1c7da2148f1c28b83b4de17edae22
Computhink-CS129 <cs129@computhink.com.sg>
2026-06-23 04:04:59 +0000

    Jun 23, 2026, 4:04 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -6,7 +6,7 @@ print("hello from day3")
 # Task 1:
 myName = input("What's your name?")
 Title = input("What's your name?")
-Command = input()
+Command = input("")
 
 
 ########################################################################
