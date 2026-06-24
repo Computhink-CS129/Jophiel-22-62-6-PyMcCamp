8c23d10281a59257985a8e87c94dc6968fc8e270
Computhink-CS129 <cs129@computhink.com.sg>
2026-06-24 02:32:07 +0000

    Jun 24, 2026, 2:32 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -39,7 +39,7 @@ print("hello from day3")
 ########################################################################
 # Task 5:
 age1=10
-age2=input=nt("How old are you?")
+age2=input=int("How old are you?")
 if age2>age1:
     print("You're old enought to enter!")
 else:
