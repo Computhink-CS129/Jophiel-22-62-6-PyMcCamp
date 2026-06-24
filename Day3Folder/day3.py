a24c43c530bc8c05108e771b8660d8386bfbee8e
Computhink-CS129 <cs129@computhink.com.sg>
2026-06-24 03:09:14 +0000

    Jun 24, 2026, 3:09 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -75,7 +75,7 @@ print("hello from day3")
 
 ########################################################################
 # Task 8:
-num1 = rn(1,202)
+num1 = random(1,202)
 num2 = random.randint(0,10)
 num3=num1/num2
 print (str(num1) + " divided by " + str(num2) + " = ?")
